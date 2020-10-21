68da962b162479535fe28d51ee30e904161f565e
Maulyanda <maulyanda94.kcb@gmail.com>
2020-10-21 22:44:31 +0700

    Update Kerjasamaa.php

--- application/modules/kerjasamaa/controllers/Kerjasamaa.php
+++ application/modules/kerjasamaa/controllers/Kerjasamaa.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class kerjasamaaa extends CI_Controller{
+class kerjasamaa extends CI_Controller{
     function __construct(){
       parent::__construct();
       //load chart_model from model
