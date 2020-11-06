de95b9f2dced31c38cb862ca92bd6a0b6d096016
unknown <dsetiono1@gmail.com>
2020-11-06 08:22:10 +0700

    sementara lagi

--- application/modules/dashboard_span/controllers/Dashboard_span.php
+++ application/modules/dashboard_span/controllers/Dashboard_span.php
@@ -1,72 +1,50 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class Pusat extends CI_Controller{
+class Dashboard_span extends CI_Controller{
     function __construct(){
       parent::__construct();
       //load chart_model from model
       $this->load->model('span_model');
     }
 
-    function index(){
-      $data = $this->span_model->get_all_pusat()->result();
-      $tanggal = $this->span_model->get_tanggal()->result();
-      $hasil_tgl = date('d F Y', strtotime($tanggal[0]->created_date));
+    function index($id = NULL){
+      if ($id == NULL) {
+        $x['title'] = 'Parent';
+        $data = $this->span_model->get_span_data()->result();
+        // var_dump($data);
+        // exit();
+        // $keuangan = $this->pok_model->jumlah_pagu()->result();
 
-      if($hasil_tgl == '01 January 1970'){
-          $hasil_tanggal = '--------';
-      }else{
-          $hasil_tanggal = $hasil_tgl;
+        // $hitung= $keuangan[0]->realisasi/$keuangan[0]->pagu*100;
+        // $persentase = round($hitung,2);
+
+        // $tanggal = $this->pok_model->get_tanggal()->result();
+        // $hasil_tgl = date('d F Y', strtotime($tanggal[0]->created_date));
+      } else {
+        $x['title'] = $id;
+        if (is_numeric($id)) {
+          $data = $this->span_model->get_out_data($id)->result();
+        } else {
+          $data = $this->span_model->get_unit_data($id)->result();
+        }
       }
 
+      // if($hasil_tgl == '01 January 1970'){
+      //     $hasil_tanggal = '--------';
+      // }else{
+      //     $hasil_tanggal = $hasil_tgl;
+      // }
+
       $x['data'] = json_encode($data);
-      $x['tanggal'] = $hasil_tanggal;
+      // $x['pagu'] = number_format($keuangan[0]->pagu);
+      // $x['realisasi'] = number_format($keuangan[0]->realisasi);
+      // $x['pengembalian'] = number_format($keuangan[0]->pengembalian);
+      // $x['sisa_pagu'] = number_format($keuangan[0]->sisa_pagu);
+      // $x['persentase'] = $persentase;
+      // $x['tanggal'] = $hasil_tanggal;
     
       $this->load->view("include/header");
       $this->load->view("view_span",$x);
       $this->load->view("include/footer");
     }
-
-    public function pusat_page()
-     {
-          // Datatables Variables
-          $draw = intval($this->input->get("draw"));
-          $start = intval($this->input->get("start"));
-          $length = intval($this->input->get("length"));
-
-          $pusats = $this->pusat_model->get_all_pusat();
-          
-    $data = array();
-    $no = 0;
-
-          foreach($pusats->result() as $r) {
-      $no++;
-               $Pagu = number_format($r->total_pagu);
-               $Realisasi = number_format($r->total_realisasi);
-               // $Pengembalian = number_format($r->Pengembalian);
-               // $Sisa_Pagu = number_format($r->Sisa_Pagu);
-               $data[] = array(
-                    $no,
-                    $link = '<button class="favorite styled"
-                    type="button">
-                    Detail
-                    </button>
-                    ',
-                    // $r->Alias,
-                    $r->ket,
-                    $Pagu,
-                    $Realisasi,
-                    // $Pengembalian,
-                    // $Sisa_Pagu,
-                    // $r->Persentase
-               );
-          }
-          
-          $output = array(
-                 "draw" => $draw,
-                 "recordsTotal" => $pusats->num_rows(),
-                 "recordsFiltered" => $pusats->num_rows(),
-                 "data" => $data
-            );
-          echo json_encode($output);
-     }
 }
--- application/modules/dashboard_span/models/span_model.php
+++ application/modules/dashboard_span/models/span_model.php
@@ -1,19 +1,25 @@
 <?php
-class Pusat_model extends CI_Model{
+class span_model extends CI_Model{
 
-	public function get_all_pusat()
+  public function get_all_dashboard()
 	{	
-		$result = $this->db->query("SELECT tbl_span.span_id, kode_satker, nama_satker SUM(output.pagu) AS total_pagu, SUM(output.realisasi) AS total_realisasi, SUM(output.persentase) AS presentase
-		FROM kampusidpn JOIN output ON span.id_b = output.id_b GROUP BY biro.id_b");
+		$result = $this->db->query("SELECT * FROM tbl_span ORDER BY created_date DESC limit 8");
         
 		return $result;
-  	}
-  
-  	public function get_tanggal()
+  }
+
+  public function jumlah_pagu()
+	{	
+		$result = $this->db->query("SELECT SUM(Pagu_t) AS pagu_t, SUM(realisasi_t) as realisasi_t, SUM(sisa) as sisa, SUM(persentase_t) as persentase_t FROM tbl_span GROUP BY created_date ORDER BY created_date DESC LIMIT 1");
+        
+		return $result;
+  }
+
+  public function get_tanggal()
 	{	
 		$result = $this->db->query("SELECT created_date FROM tbl_span ORDER BY created_date DESC limit 1");
         
 		return $result;
-	}
- 
+  }
+  
 }
