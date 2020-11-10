e350f2610f504f9390df240d67f95baef5cc26c0
Dicky Setiono <dsetiono1@gmail.com>
2020-11-10 17:02:45 +0700

    Update Uploads.php

--- application/modules/uploads/controllers/Uploads.php
+++ application/modules/uploads/controllers/Uploads.php
@@ -1416,7 +1416,7 @@ class Uploads extends CI_Controller {
         }
     }
     
-            public function uploadSpan()
+           public function uploadSpan()
     {
                 // Load plugin PHPExcel nya
         include APPPATH.'third_party/PHPExcel/PHPExcel.php';
@@ -1438,7 +1438,10 @@ class Uploads extends CI_Controller {
             redirect('uploads/');
 
         } else {
-            
+
+
+            $data_upload = $this->upload->data(); 
+
             $excelreader       = new PHPExcel_Reader_Excel2007();
             $loadexcel         = $excelreader->load('excel/'.$data_upload['file_name']); // Load file yang telah diupload ke folder excel
             $sheet             = $loadexcel->getSheetByName("Sheet1")->toArray(null, true, true ,true);
@@ -1447,67 +1450,69 @@ class Uploads extends CI_Controller {
             $biro = array();
             $date = new DateTime();
             $datee = $date->format('Y-m-d');
-            for($i = 10;$i < 23;$i++)
-    {
-        // var_dump($sheetData[$i]['2'] != 1286);
-        if($sheetData[$i]['2'] != NULL && $sheetData[$i]['2'] != 1286 && $sheetData[$i]['2'] != 1292 && $sheetData[$i]['2'] != 1293 && $sheetData[$i]['2'] != 1294 && $sheetData[$i]['2'] != 1295){
-            $data = array();
-            array_push($data, array(
-                'kode_satker'      => $sheetData[$i]['2'],
-                'nama_satker'      => $sheetData[$i]['3'],
-                'pagu_bp'      => preg_replace("/[^0-9]/", "", $sheetData[$i]['4']),
-                'realisasi_bp'      => preg_replace("/[^0-9]/", "", $sheetData[$i]['5']),
-                'persentase_bp'      => $sheetData[$i]['7'],
-                'pagu_bb'      => preg_replace("/[^0-9]/", "", $sheetData[$i]['8']),
-                'realisasi_bb'   => preg_replace("/[^0-9]/", "", $sheetData[$i]['9']),
-                'persentase_bb'   => $sheetData[$i]['11'],
-                'pagu_bm'   => preg_replace("/[^0-9]/", "", $sheetData[$i]['12']),
-                'realisasi_bm'   => preg_replace("/[^0-9]/", "", $sheetData[$i]['13']),
-                'persentase_bm'   => $sheetData[$i]['15'],
-                'pagu_t'   => preg_replace("/[^0-9]/", "", $sheetData[$i]['16']),
-                'realisasi_t'   => preg_replace("/[^0-9]/", "", $sheetData[$i]['17']),
-                'persentase_t'   => $sheetData[$i]['19'],
-                'sisa'   => preg_replace("/[^0-9]/", "", $sheetData[$i]['20']),
-                'tgl' => $datee,
-            ));
-        }
-    }
+            $numrow = 1;
+            foreach($sheet as $row){
+                if($numrow > 1){
+                    $kode_satker = trim($row['A']);
+                    array_push($data, array(
+                        'kode_satker'      => $kode_satker,
+                        'nama_satker'      => preg_replace("/[^0-9]/", "", $row['B']),
+                        'pagu_bp'      => preg_replace("/[^0-9]/", "", $row['C']),
+                        'realisasi_bp'      => preg_replace("/[^0-9]/", "", $row['D']),
+                        'persentase_bp'      => preg_replace("/[^0-9]/", "", $row['E']),
+                        'pagu_bb'      => preg_replace("/[^0-9]/", "", $row['F']),
+                        'realisasi_bb'      => preg_replace("/[^0-9]/", "", $row['G']),
+                        'persentase_bb'      => preg_replace("/[^0-9]/", "", $row['H']),
+                        'pagu_bm'      => preg_replace("/[^0-9]/", "", $row['I']),
+                        'realisasi_bm'      => preg_replace("/[^0-9]/", "", $row['J']),
+                        'persentase_bm'      => preg_replace("/[^0-9]/", "", $row['K']),
+                        'pagu_t'      => preg_replace("/[^0-9]/", "", $row['L']),
+                        'realisasi_t'      => preg_replace("/[^0-9]/", "", $row['M']),
+                        'persentase_t'      => preg_replace("/[^0-9]/", "", $row['N']),
+                        'persentase_bb'      => preg_replace("/[^0-9]/", "", $row['O']),
+                        'sisa'      => preg_replace("/[^0-9]/", "", $row['P']),
+                        '%'      => preg_replace("/[^0-9]/", "", $row['Q'])
 
-    for($i = 11;$i < 15;$i++)
-    {
-        $biro = array();
-        array_push($biro, array(
-            'kode_satker'      => $sheetData[$i]['2'],
-            'nama_satker'      => $sheetData[$i]['3'],
-            'pagu_bp'      => preg_replace("/[^0-9]/", "", $sheetData[$i]['4']),
-            'realisasi_bp'      => preg_replace("/[^0-9]/", "", $sheetData[$i]['5']),
-            'persentase_bp'      => $sheetData[$i]['7'],
-            'pagu_bb'      => preg_replace("/[^0-9]/", "", $sheetData[$i]['8']),
-            'realisasi_bb'   => preg_replace("/[^0-9]/", "", $sheetData[$i]['9']),
-            'persentase_bb'   => $sheetData[$i]['11'],
-            'pagu_bm'   => preg_replace("/[^0-9]/", "", $sheetData[$i]['12']),
-            'realisasi_bm'   => preg_replace("/[^0-9]/", "", $sheetData[$i]['13']),
-            'persentase_bm'   => $sheetData[$i]['15'],
-            'pagu_t'   => preg_replace("/[^0-9]/", "", $sheetData[$i]['16']),
-            'realisasi_t'   => preg_replace("/[^0-9]/", "", $sheetData[$i]['17']),
-            'persentase_t'   => $sheetData[$i]['19'],
-            'sisa'   => preg_replace("/[^0-9]/", "", $sheetData[$i]['20']),
-            'tgl' => $datee,
-        ));
-    }
-        $this->db->truncate('tbl_span_biro');
-        $this->db->insert_batch('tbl_span_biro', $biro);
-        $this->db->truncate('tbl_span');
-        $this->db->insert_batch('tbl_span', $data);
+                    ));     
+                }
+            }
+            {
+                array_push($biro, array(
+                    'kode_satker'      => $kode_satker,
+                    'nama_satker'      => preg_replace("/[^0-9]/", "", $row['B']),
+                    'pagu_bp'      => preg_replace("/[^0-9]/", "", $row['C']),
+                    'realisasi_bp'      => preg_replace("/[^0-9]/", "", $row['D']),
+                    'persentase_bp'      => preg_replace("/[^0-9]/", "", $row['E']),
+                    'pagu_bb'      => preg_replace("/[^0-9]/", "", $row['F']),
+                    'realisasi_bb'      => preg_replace("/[^0-9]/", "", $row['G']),
+                    'persentase_bb'      => preg_replace("/[^0-9]/", "", $row['H']),
+                    'pagu_bm'      => preg_replace("/[^0-9]/", "", $row['I']),
+                    'realisasi_bm'      => preg_replace("/[^0-9]/", "", $row['J']),
+                    'persentase_bm'      => preg_replace("/[^0-9]/", "", $row['K']),
+                    'pagu_t'      => preg_replace("/[^0-9]/", "", $row['L']),
+                    'realisasi_t'      => preg_replace("/[^0-9]/", "", $row['M']),
+                    'persentase_t'      => preg_replace("/[^0-9]/", "", $row['N']),
+                    'persentase_bb'      => preg_replace("/[^0-9]/", "", $row['O']),
+                    'sisa'      => preg_replace("/[^0-9]/", "", $row['P']),
+                    'tgl' => $datee,
+                ));
+            }
+            
+                
+            $this->db->truncate('tbl_span_biro');
+            $this->db->insert_batch('tbl_span_biro', $biro);
+            $this->db->truncate('tbl_span');
+            $this->db->insert_batch('tbl_span', $data);
             //delete file from server
             unlink(realpath('excel/'.$data_upload['file_name']));
 
-           //upload success
-           $this->session->set_flashdata('notifspan', '<div class="alert alert-success"><b>PROSES IMPORT BERHASIL!</b> Data berhasil diimport!</div>');
-           //redirect halaman
-           redirect('uploads/');
-       }
-   }
+            //upload success
+            $this->session->set_flashdata('notifspan', '<div class="alert alert-success"><b>PROSES IMPORT BERHASIL!</b> Data berhasil diimport!</div>');
+            //redirect halaman
+            redirect('uploads/');
+            
+        }
+    }
 
 
 
